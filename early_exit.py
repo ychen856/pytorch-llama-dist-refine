8ec57477851214d4e6ce4faccbe1e8f7c958af57
ychen514 <ychen514@uic.edu>
2025-06-19 22:24:30 -0800

    add test

--- early_exit.py
+++ early_exit.py
@@ -331,7 +331,7 @@ def early_exit_lm_head(lm_models, out, lm_head):
 
 def early_exit_regression(lm_models, out, lm_head, threshold=0.9):
     threshold = 0.5
-    temperature = 0.6
+    temperature = 0.3
 
     print('head: ', lm_head)
 
