c3b1c59afe8f4c5c60a5c135b129cb7316ad093f
ychen514 <ychen514@uic.edu>
2025-07-02 00:54:37 -0800

    add test

--- run_lm.sh
+++ run_lm.sh
@@ -1,25 +1,31 @@
 #!/bin/sh
 
-python3 lm_inference.py --head 1 --ppl 20 --config config_nrp.yaml 2>&1|tee lm_inference_head_1_ppl_20_1.log
-python3 lm_inference.py --head 1 --ppl 20 --config config_nrp.yaml 2>&1|tee lm_inference_head_1_ppl_20_2.log
-python3 lm_inference.py --head 1 --ppl 20 --config config_nrp.yaml 2>&1|tee lm_inference_head_1_ppl_20_3.log
-python3 lm_inference.py --head 1 --ppl 20 --config config_nrp.yaml 2>&1|tee lm_inference_head_1_ppl_20_4.log
-python3 lm_inference.py --head 1 --ppl 20 --config config_nrp.yaml 2>&1|tee lm_inference_head_1_ppl_20_5.log
+python3 lm_inference.py --head 4 --ppl 10 --config config_nrp.yaml 2>&1|tee lm_inference_head_4_ppl_10_1.log
+python3 lm_inference.py --head 4 --ppl 10 --config config_nrp.yaml 2>&1|tee lm_inference_head_4_ppl_10_2.log
+python3 lm_inference.py --head 4 --ppl 10 --config config_nrp.yaml 2>&1|tee lm_inference_head_4_ppl_10_3.log
+python3 lm_inference.py --head 4 --ppl 10 --config config_nrp.yaml 2>&1|tee lm_inference_head_4_ppl_10_4.log
+python3 lm_inference.py --head 4 --ppl 10 --config config_nrp.yaml 2>&1|tee lm_inference_head_4_ppl_10_5.log
 
-python3 lm_inference.py --head 2 --ppl 20 --config config_nrp.yaml 2>&1|tee lm_inference_head_2_ppl_20_1.log
-python3 lm_inference.py --head 2 --ppl 20 --config config_nrp.yaml 2>&1|tee lm_inference_head_2_ppl_20_2.log
-python3 lm_inference.py --head 2 --ppl 20 --config config_nrp.yaml 2>&1|tee lm_inference_head_2_ppl_20_3.log
-python3 lm_inference.py --head 2 --ppl 20 --config config_nrp.yaml 2>&1|tee lm_inference_head_2_ppl_20_4.log
-python3 lm_inference.py --head 2 --ppl 20 --config config_nrp.yaml 2>&1|tee lm_inference_head_2_ppl_20_5.log
+python3 lm_inference.py --head 1 --ppl 30 --config config_nrp.yaml 2>&1|tee lm_inference_head_1_ppl_30_1.log
+python3 lm_inference.py --head 1 --ppl 30 --config config_nrp.yaml 2>&1|tee lm_inference_head_1_ppl_30_2.log
+python3 lm_inference.py --head 1 --ppl 30 --config config_nrp.yaml 2>&1|tee lm_inference_head_1_ppl_30_3.log
+python3 lm_inference.py --head 1 --ppl 30 --config config_nrp.yaml 2>&1|tee lm_inference_head_1_ppl_30_4.log
+python3 lm_inference.py --head 1 --ppl 30 --config config_nrp.yaml 2>&1|tee lm_inference_head_1_ppl_30_5.log
 
-python3 lm_inference.py --head 4 --ppl 20 --config config_nrp.yaml 2>&1|tee lm_inference_head_4_ppl_20_1.log
-python3 lm_inference.py --head 4 --ppl 20 --config config_nrp.yaml 2>&1|tee lm_inference_head_4_ppl_20_2.log
-python3 lm_inference.py --head 4 --ppl 20 --config config_nrp.yaml 2>&1|tee lm_inference_head_4_ppl_20_3.log
-python3 lm_inference.py --head 4 --ppl 20 --config config_nrp.yaml 2>&1|tee lm_inference_head_4_ppl_20_4.log
-python3 lm_inference.py --head 4 --ppl 20 --config config_nrp.yaml 2>&1|tee lm_inference_head_4_ppl_20_5.log
+python3 lm_inference.py --head 2 --ppl 30 --config config_nrp.yaml 2>&1|tee lm_inference_head_2_ppl_30_1.log
+python3 lm_inference.py --head 2 --ppl 30 --config config_nrp.yaml 2>&1|tee lm_inference_head_2_ppl_30_2.log
+python3 lm_inference.py --head 2 --ppl 30 --config config_nrp.yaml 2>&1|tee lm_inference_head_2_ppl_30_3.log
+python3 lm_inference.py --head 2 --ppl 30 --config config_nrp.yaml 2>&1|tee lm_inference_head_2_ppl_30_4.log
+python3 lm_inference.py --head 2 --ppl 30 --config config_nrp.yaml 2>&1|tee lm_inference_head_2_ppl_30_5.log
 
-python3 lm_inference.py --head 6 --ppl 20 --config config_nrp.yaml 2>&1|tee lm_inference_head_6_ppl_20_1.log
-python3 lm_inference.py --head 6 --ppl 20 --config config_nrp.yaml 2>&1|tee lm_inference_head_6_ppl_20_2.log
-python3 lm_inference.py --head 6 --ppl 20 --config config_nrp.yaml 2>&1|tee lm_inference_head_6_ppl_20_3.log
-python3 lm_inference.py --head 6 --ppl 20 --config config_nrp.yaml 2>&1|tee lm_inference_head_6_ppl_20_4.log
-python3 lm_inference.py --head 6 --ppl 20 --config config_nrp.yaml 2>&1|tee lm_inference_head_6_ppl_20_5.log
+python3 lm_inference.py --head 4 --ppl 30 --config config_nrp.yaml 2>&1|tee lm_inference_head_4_ppl_30_1.log
+python3 lm_inference.py --head 4 --ppl 30 --config config_nrp.yaml 2>&1|tee lm_inference_head_4_ppl_30_2.log
+python3 lm_inference.py --head 4 --ppl 30 --config config_nrp.yaml 2>&1|tee lm_inference_head_4_ppl_30_3.log
+python3 lm_inference.py --head 4 --ppl 30 --config config_nrp.yaml 2>&1|tee lm_inference_head_4_ppl_30_4.log
+python3 lm_inference.py --head 4 --ppl 30 --config config_nrp.yaml 2>&1|tee lm_inference_head_4_ppl_30_5.log
+
+python3 lm_inference.py --head 6 --ppl 30 --config config_nrp.yaml 2>&1|tee lm_inference_head_6_ppl_30_1.log
+python3 lm_inference.py --head 6 --ppl 30 --config config_nrp.yaml 2>&1|tee lm_inference_head_6_ppl_30_2.log
+python3 lm_inference.py --head 6 --ppl 30 --config config_nrp.yaml 2>&1|tee lm_inference_head_6_ppl_30_3.log
+python3 lm_inference.py --head 6 --ppl 30 --config config_nrp.yaml 2>&1|tee lm_inference_head_6_ppl_30_4.log
+python3 lm_inference.py --head 6 --ppl 30 --config config_nrp.yaml 2>&1|tee lm_inference_head_6_ppl_30_5.log
