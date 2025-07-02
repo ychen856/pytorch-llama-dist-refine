ad4a2a0af1fae9375e22693843f3781ff380c762
ychen514 <ychen514@uic.edu>
2025-07-02 00:29:50 -0800

    add test

--- client_jetson.py
+++ client_jetson.py
@@ -566,16 +566,19 @@ def task2_computation(models, lm_models, start_idx, end_idx, end_idx_buff, head_
             #print('statistic: ', statistics_period)
             if (input_count) % 2 == 0 and input_count < 12 and end_idx < max_layers and statistics_period <= 10:
             #if (input_count) % 4 == 0 and input_count < 24 and end_idx < max_layers and statistics_period <= 20:
-                print('testing higher value(i<30)')
+                print('testing higher value(i<12)')
+                logger.log(f'testing higher value(i<30)')
                 performance_data_store.max_end_idx = end_idx
                 end_idx = end_idx + 1
 
             if cycle_count == (statistics_period - 6) and input_count >= 12 and cycle_count % 2 == 0:
-                print('testing lower value (i>30)')
+                print('testing lower value (i>12)')
+                logger.log(f'testing lower value (i>30)')
                 end_idx = max(1, end_idx - 1)
 
             if cycle_count > (statistics_period - 6) and input_count >= 12 and end_idx < max_layers and cycle_count % 2 == 0:
-                print('testing higher value (i>30): ')
+                print('testing higher value (i>30)')
+                logger.log(f'testing higher value (i>30)')
                 performance_data_store.max_end_idx = end_idx
                 end_idx = end_idx + 1
 
