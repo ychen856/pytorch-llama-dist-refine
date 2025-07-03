44cca980791b4cfcd6cf692a82424228cb6d1687
ychen514 <ychen514@uic.edu>
2025-07-03 13:01:12 -0800

    add test

--- run.sh
+++ run.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 
 sleep 10
-python3.8 -u client_jetson.py --config config_jetson2.yaml --ppl 30 --weight 0.0 --log jetson_2_device_wlan2_ppl_30_weight_0_2.log
+python3.8 -u client_jetson.py --config config_jetson2.yaml --ppl 30 --weight 0.0 --log jetson_2_device_wlan2_ppl_30_weight_0_4.log
 sleep 10
-python3.8 -u client_jetson.py --config config_jetson2.yaml --ppl 30 --weight 0.2 --log jetson_2_device_wlan2_ppl_30_weight_2_2.log
+python3.8 -u client_jetson.py --config config_jetson2.yaml --ppl 30 --weight 0.2 --log jetson_2_device_wlan2_ppl_30_weight_2_4.log
 sleep 10
-python3.8 -u client_jetson.py --config config_jetson2.yaml --ppl 30 --weight 0.4 --log jetson_2_device_wlan2_ppl_30_weight_4_2.log
+python3.8 -u client_jetson.py --config config_jetson2.yaml --ppl 30 --weight 0.4 --log jetson_2_device_wlan2_ppl_30_weight_4_4.log
 sleep 10
-python3.8 -u client_jetson.py --config config_jetson2.yaml --ppl 30 --weight 0.6 --log jetson_2_device_wlan2_ppl_30_weight_6_2.log
+python3.8 -u client_jetson.py --config config_jetson2.yaml --ppl 30 --weight 0.6 --log jetson_2_device_wlan2_ppl_30_weight_6_4.log
 
 sleep 10
-python3.8 -u client_jetson.py --config config_jetson2.yaml --ppl 30 --weight 0.0 --log jetson_2_device_wlan2_ppl_30_weight_0_3.log
+python3.8 -u client_jetson.py --config config_jetson2.yaml --ppl 30 --weight 0.0 --log jetson_2_device_wlan2_ppl_30_weight_0_5.log
 sleep 10
-python3.8 -u client_jetson.py --config config_jetson2.yaml --ppl 30 --weight 0.2 --log jetson_2_device_wlan2_ppl_30_weight_2_3.log
+python3.8 -u client_jetson.py --config config_jetson2.yaml --ppl 30 --weight 0.2 --log jetson_2_device_wlan2_ppl_30_weight_2_5.log
 sleep 10
-python3.8 -u client_jetson.py --config config_jetson2.yaml --ppl 30 --weight 0.4 --log jetson_2_device_wlan2_ppl_30_weight_4_3.log
+python3.8 -u client_jetson.py --config config_jetson2.yaml --ppl 30 --weight 0.4 --log jetson_2_device_wlan2_ppl_30_weight_4_5.log
 sleep 10
-python3.8 -u client_jetson.py --config config_jetson2.yaml --ppl 30 --weight 0.6 --log jetson_2_device_wlan2_ppl_30_weight_6_3.log
+python3.8 -u client_jetson.py --config config_jetson2.yaml --ppl 30 --weight 0.6 --log jetson_2_device_wlan2_ppl_30_weight_6_5.log
 
 #sleep 10
 #python3.8 -u client_jetson.py --config config_jetson2.yaml --ppl 10 --weight 0.0 --log jetson_2_device_wlan2_ppl_10_weight_0_1.log
