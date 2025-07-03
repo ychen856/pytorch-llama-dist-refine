3fd0d5933c31989a575bcd506c091ba324b74f77
ychen514 <ychen514@uic.edu>
2025-07-03 01:44:07 -0800

    add test

--- run_lm.sh
+++ run_lm.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-#python3 lm_inference.py --head 6 --ppl 20 --config config_nrp.yaml 2>&1|tee lm_inference_head_6_ppl_20_4.log
-#python3 lm_inference.py --head 6 --ppl 20 --config config_nrp.yaml 2>&1|tee lm_inference_head_6_ppl_20_5.log
+python3 lm_inference.py --head 6 --ppl 10 --config config_nrp.yaml 2>&1|tee lm_inference_head_6_ppl_10_1.log
+python3 lm_inference.py --head 6 --ppl 10 --config config_nrp.yaml 2>&1|tee lm_inference_head_6_ppl_10_2.log
+python3 lm_inference.py --head 6 --ppl 10 --config config_nrp.yaml 2>&1|tee lm_inference_head_6_ppl_10_3.log
+python3 lm_inference.py --head 6 --ppl 10 --config config_nrp.yaml 2>&1|tee lm_inference_head_6_ppl_10_4.log
+python3 lm_inference.py --head 6 --ppl 10 --config config_nrp.yaml 2>&1|tee lm_inference_head_6_ppl_10_5.log
+
 #python3 lm_inference.py --head 4 --ppl 10 --config config_nrp.yaml 2>&1|tee lm_inference_head_4_ppl_10_1.log
 #python3 lm_inference.py --head 4 --ppl 10 --config config_nrp.yaml 2>&1|tee lm_inference_head_4_ppl_10_2.log
 #python3 lm_inference.py --head 4 --ppl 10 --config config_nrp.yaml 2>&1|tee lm_inference_head_4_ppl_10_3.log
