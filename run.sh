b01f26c03880a9411db81181df1598c9925e3001
ychen514 <ychen514@uic.edu>
2025-07-05 14:52:06 -0800

    add test

--- run.sh
+++ run.sh
@@ -1,50 +1,5 @@
 #!/bin/sh
 
-sleep 10
-python3.8 client_jetson_adp.py --ppl 30 --config config_jetson2.yaml --log jetson2_wlan_ppl_30_fixed_1.log
-sleep 10
-python3.8 client_jetson_adp.py --ppl 30 --config config_jetson2.yaml --log jetson2_wlan_ppl_30_fixed_2.log
-sleep 10
-python3.8 client_jetson_adp.py --ppl 30 --config config_jetson2.yaml --log jetson2_wlan_ppl_30_fixed_3.log
-sleep 10
-python3.8 client_jetson_adp.py --ppl 30 --config config_jetson2.yaml --log jetson2_wlan_ppl_30_fixed_4.log
-sleep 10
-python3.8 client_jetson_adp.py --ppl 30 --config config_jetson2.yaml --log jetson2_wlan_ppl_30_fixed_5.log
-sleep 10
-python3.8 client_jetson_adp.py --ppl 30 --config config_jetson2.yaml --log jetson2_wlan_ppl_30_fixed_6.log
-sleep 10
-python3.8 client_jetson_adp.py --ppl 30 --config config_jetson2.yaml --log jetson2_wlan_ppl_30_fixed_7.log
-sleep 10
-python3.8 client_jetson_adp.py --ppl 30 --config config_jetson2.yaml --log jetson2_wlan_ppl_30_fixed_8.log
-
-
-sleep 10
-python3.8 client_jetson_adp.py --ppl 20 --config config_jetson2.yaml --log jetson2_wlan_ppl_20_fixed_1.log
-sleep 10
-python3.8 client_jetson_adp.py --ppl 20 --config config_jetson2.yaml --log jetson2_wlan_ppl_20_fixed_2.log
-sleep 10
-python3.8 client_jetson_adp.py --ppl 20 --config config_jetson2.yaml --log jetson2_wlan_ppl_20_fixed_3.log
-sleep 10
-python3.8 client_jetson_adp.py --ppl 20 --config config_jetson2.yaml --log jetson2_wlan_ppl_20_fixed_4.log
-sleep 10
-python3.8 client_jetson_adp.py --ppl 20 --config config_jetson2.yaml --log jetson2_wlan_ppl_20_fixed_5.log
-sleep 10
-python3.8 client_jetson_adp.py --ppl 20 --config config_jetson2.yaml --log jetson2_wlan_ppl_20_fixed_6.log
-sleep 10
-python3.8 client_jetson_adp.py --ppl 20 --config config_jetson2.yaml --log jetson2_wlan_ppl_20_fixed_7.log
-sleep 10
-python3.8 client_jetson_adp.py --ppl 20 --config config_jetson2.yaml --log jetson2_wlan_ppl_20_fixed_8.log
-
-
-sleep 10
-python3.8 client_jetson_adp.py --ppl 10 --config config_jetson2.yaml --log jetson2_wlan_ppl_10_fixed_1.log
-sleep 10
-python3.8 client_jetson_adp.py --ppl 10 --config config_jetson2.yaml --log jetson2_wlan_ppl_10_fixed_2.log
-sleep 10
-python3.8 client_jetson_adp.py --ppl 10 --config config_jetson2.yaml --log jetson2_wlan_ppl_10_fixed_3.log
-sleep 10
-python3.8 client_jetson_adp.py --ppl 10 --config config_jetson2.yaml --log jetson2_wlan_ppl_10_fixed_4.log
-sleep 10
 python3.8 client_jetson_adp.py --ppl 10 --config config_jetson2.yaml --log jetson2_wlan_ppl_10_fixed_5.log
 sleep 10
 python3.8 client_jetson_adp.py --ppl 10 --config config_jetson2.yaml --log jetson2_wlan_ppl_10_fixed_6.log
