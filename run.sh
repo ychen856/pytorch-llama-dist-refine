badda300d32ee7a7c838151fd66a579791c9edc7
ychen514 <ychen514@uic.edu>
2025-07-05 15:11:41 -0800

    add test

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-
+python3.8 client_jetson_adp.py --ppl 10 --config config_jetson2.yaml --log test.log
+sleep 10
 python3.8 client_jetson_adp.py --ppl 10 --config config_jetson2.yaml --log jetson2_wlan_ppl_10_fixed_5.log
 sleep 10
 python3.8 client_jetson_adp.py --ppl 10 --config config_jetson2.yaml --log jetson2_wlan_ppl_10_fixed_6.log
