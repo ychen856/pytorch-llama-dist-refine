fe0fd81ce16f364e84192c9ac53f69d9a8d09ba4
ychen514 <ychen514@uic.edu>
2025-07-07 23:05:38 -0800

    add test

--- run.sh
+++ run.sh
@@ -1,28 +1,26 @@
 #!/bin/sh
-python3.8 client_jetson_adp.py --ppl 20 --mode default --config config_jetson2.yaml --log test.log
+python3.8 client_jetson_adp.py --ppl 10 --mode default --config config_jetson2.yaml --log test.log
 sleep 10
 
-python3.8 client_jetson_adp.py --ppl 20 --mode default --config config_jetson2.yaml --log 07_jetson2_wlan_ppl_20_fixed_1.log
+python3.8 client_jetson_adp.py --ppl 10 --mode default --config config_jetson2.yaml --log 07_jetson2_wlan_ppl_10_fixed_1.log
 sleep 10
-python3.8 client_jetson_adp.py --ppl 20 --mode fixed --config config_jetson2.yaml --log 07_jetson2_wlan_ppl_20_fixed_1.log
+python3.8 client_jetson_adp.py --ppl 10 --mode fixed --config config_jetson2.yaml --log 07_jetson2_wlan_ppl_10_fixed_1.log
 sleep 10
-python3.8 client_jetson_adp.py --ppl 20 --mode linear-exit-rate --config config_jetson2.yaml --log 07_jetson2_wlan_ppl_20_linear_1.log
+python3.8 client_jetson_adp.py --ppl 10 --mode linear-exit-rate --config config_jetson2.yaml --log 07_jetson2_wlan_ppl_10_linear_1.log
 sleep 10
-python3.8 client_jetson_adp.py --ppl 20 --mode exit-rate --config config_jetson2.yaml --log 07_jetson2_wlan_ppl_20_exit_1.log
+python3.8 client_jetson_adp.py --ppl 10 --mode exit-rate --config config_jetson2.yaml --log 07_jetson2_wlan_ppl_10_exit_1.log
 sleep 10
-python3.8 client_jetson_adp.py --ppl 20 --mode bandwidth-aware --config config_jetson2.yaml --log 07_jetson2_wlan_ppl_20_bandwidth_1.log
-sleep 10
-
-python3.8 client_jetson_adp.py --ppl 30 --mode default --config config_jetson2.yaml --log 07_jetson2_wlan_ppl_30_fixed_1.log
+python3.8 client_jetson_adp.py --ppl 10 --mode bandwidth-aware --config config_jetson2.yaml --log 07_jetson2_wlan_ppl_10_bandwidth_1.log
 sleep 10
 
-python3.8 client_jetson_adp.py --ppl 20 --mode default --config config_jetson2.yaml --log 07_jetson2_wlan_ppl_20_fixed_2.log
+
+python3.8 client_jetson_adp.py --ppl 10 --mode default --config config_jetson2.yaml --log 07_jetson2_wlan_ppl_10_default_2.log
 sleep 10
-python3.8 client_jetson_adp.py --ppl 20 --mode fixed --config config_jetson2.yaml --log 07_jetson2_wlan_ppl_20_fixed_2.log
+python3.8 client_jetson_adp.py --ppl 10 --mode fixed --config config_jetson2.yaml --log 07_jetson2_wlan_ppl_10_fixed_2.log
 sleep 10
-python3.8 client_jetson_adp.py --ppl 20 --mode linear-exit-rate --config config_jetson2.yaml --log 07_jetson2_wlan_ppl_20_linear_2.log
+python3.8 client_jetson_adp.py --ppl 10 --mode linear-exit-rate --config config_jetson2.yaml --log 07_jetson2_wlan_ppl_10_linear_2.log
 sleep 10
-python3.8 client_jetson_adp.py --ppl 20 --mode exit-rate --config config_jetson2.yaml --log 07_jetson2_wlan_ppl_20_exit_2.log
+python3.8 client_jetson_adp.py --ppl 10 --mode exit-rate --config config_jetson2.yaml --log 07_jetson2_wlan_ppl_10_exit_2.log
 sleep 10
-python3.8 client_jetson_adp.py --ppl 20 --mode bandwidth-aware --config config_jetson2.yaml --log 07_jetson2_wlan_ppl_20_bandwidth_2.log
+python3.8 client_jetson_adp.py --ppl 10 --mode bandwidth-aware --config config_jetson2.yaml --log 07_jetson2_wlan_ppl_10_bandwidth_2.log
 sleep 10
