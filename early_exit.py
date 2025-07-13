08c5d784ea555cafaaf508b7b5b61aec0bd167d2
ychen514 <ychen514@uic.edu>
2025-07-12 17:09:41 -0800

    add test

--- early_exit.py
+++ early_exit.py
@@ -390,7 +390,7 @@ def early_exit_lm_head_shift(lm_models, out, lm_head, ppl):
         elif lm_head == 6:
             early_rate = 0.6689
         elif lm_head == 3:
-            early_rate = 0.66506
+            early_rate = 0.65506
         elif lm_head == 4:
             early_rate = 0.66406
         elif lm_head == 2:
