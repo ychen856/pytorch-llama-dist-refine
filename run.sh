673c4f378849c5b58c21a1efd6e67fc5fe3755eb
ychen514 <ychen514@uic.edu>
2025-07-12 22:42:44 -0800

    add test

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@
 python3.8 client_jetson_adp.py --ppl 10 --mode default --config config_jetson2.yaml --log test.log
 sleep 10
 
-python3.8 client_jetson_adp.py --ppl 10 --mode exit-rate --config config_jetson2.yaml --log 11_jetson2_wlan_ppl_10_mid_exit_TTT_4.log
+python3.8 client_jetson_adp.py --ppl 10 --mode exit-rate --config config_jetson2.yaml --log 11_jetson2_wlan_ppl_10_mid_exit_TTT_5.log
 sleep 10
-python3.8 client_jetson_adp.py --ppl 20 --mode exit-rate --config config_jetson2.yaml --log 11_jetson2_wlan_ppl_10_mid_exit_TTT_4.log
+python3.8 client_jetson_adp.py --ppl 20 --mode exit-rate --config config_jetson2.yaml --log 11_jetson2_wlan_ppl_10_mid_exit_TTT_5.log
 sleep 10
-python3.8 client_jetson_adp.py --ppl 30 --mode exit-rate --config config_jetson2.yaml --log 11_jetson2_wlan_ppl_10_mid_exit_TTT_4.log
+python3.8 client_jetson_adp.py --ppl 30 --mode exit-rate --config config_jetson2.yaml --log 11_jetson2_wlan_ppl_10_mid_exit_TTT_5.log
 sleep 10
