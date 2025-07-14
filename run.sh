0caac756332fdb654376793c8a04c4f85db13e98
ychen514 <ychen514@uic.edu>
2025-07-13 19:51:56 -0800

    add test

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
   #!/bin/sh
-python3.8 client_jetson_adp.py --ppl 10 --mode default --config config_jetson2.yaml --log test.log
-sleep 10
+#python3.8 client_jetson_adp.py --ppl 10 --mode default --config config_jetson2.yaml --log test.log
+#sleep 10
 
 #python3.8 client_jetson_adp.py --ppl 10 --mode exit-rate --config config_jetson2.yaml --log 13_jetson2_wlan_ppl_10_short_exit_TTT_3.log
 #sleep 10
