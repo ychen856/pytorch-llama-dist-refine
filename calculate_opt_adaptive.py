1d7e9379e80bcc9efe4c0f016eea48bd6dd6a199
ychen514 <ychen514@uic.edu>
2025-07-13 19:51:25 -0800

    add test

--- calculate_opt_adaptive.py
+++ calculate_opt_adaptive.py
@@ -443,12 +443,12 @@ def calculate_opt(data_store: PerformanceDataStore, ppl, lm_manager, mode, shock
         individual_latencies_with_timestamps_not_early.sort(key=lambda x: x[1])
         individual_latencies_with_timestamps_is_early.sort(key=lambda x: x[1])
 
-        if len(individual_latencies_with_timestamps_not_early) > 1:
+        '''if len(individual_latencies_with_timestamps_not_early) > 1:
             max_latency_entry = max(individual_latencies_with_timestamps_not_early, key=lambda x: x[0])
             individual_latencies_with_timestamps_not_early.remove(max_latency_entry)
         if len(individual_latencies_with_timestamps_is_early) > 1:
             max_latency_entry = max(individual_latencies_with_timestamps_is_early, key=lambda x: x[0])
-            individual_latencies_with_timestamps_is_early.remove(max_latency_entry)
+            individual_latencies_with_timestamps_is_early.remove(max_latency_entry)'''
 
         weighted_sum_for_path = 0.0
         total_weight_for_path = 0.0
