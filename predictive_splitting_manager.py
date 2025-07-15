2506f418b4300a99e0120aacdc3cfffc00e7e137
ychen514 <ychen514@uic.edu>
2025-07-15 01:27:48 -0800

    add test

--- predictive_splitting_manager.py
+++ predictive_splitting_manager.py
@@ -58,8 +58,8 @@ class PredictiveSplittingManager:
         self.logger.log(f'HISTORY!!!!!!!!!! {sum(self.history)}')
         self.logger.log(f'avg client: {sum(self.avg_client)}')
         self.logger.log(f'length: {len(self.avg_client)}')
-        self.logger.log(f'AVERAGE!!!!!!!!!! {(sum(self.avg_client) + sum(self.avg_comm) + sum(self.avg_server))}')
-        return sum(self.history) >= self.shock_threshold * (sum(self.avg_client) + sum(self.avg_comm) + sum(self.avg_server))
+        self.logger.log(f'AVERAGE!!!!!!!!!! {(sum(self.avg_client) + sum(self.avg_comm) + sum(self.avg_server)) / len(self.avg_client)}')
+        return sum(self.history) >= self.shock_threshold * (sum(self.avg_client) + sum(self.avg_comm) + sum(self.avg_server)) / len(self.avg_client)
 
     def decide_k(self, k_opt):
         shock_c, shock_m, shock_s = self.last_shock_flags
