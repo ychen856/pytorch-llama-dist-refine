c3034a64487bfd068d932228cd2bf72bee5ddb30
ychen514 <ychen514@uic.edu>
2025-07-14 19:38:50 -0800

    add test

--- client_jetson_adp.py
+++ client_jetson_adp.py
@@ -675,10 +675,10 @@ def task2_computation(models, lm_models, start_idx, end_idx, end_idx_buff, head_
 
             cycle_count = 0
 
-        '''# test...
+        # test...
         end_idx = 4
         end_idx_buff = 5
-        # test...'''
+        # test...
 
         lm_head, _ = get_lm_head_idx(end_idx)
         if not lm_head == head_idx:
@@ -720,10 +720,10 @@ if __name__ == '__main__':
     end_idx_buff = args.end_idx_buff
     head_idx = 2
 
-    '''#test...
+    #test...
     end_idx = 4
     end_idx_buff = 5
-    # test...'''
+    # test...
 
     performance_data_store.statistic_period = 20
     performance_data_store.end_idx = end_idx
--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 #python3.8 client_jetson_adp.py --ppl 10 --mode default --config config_jetson2.yaml --log test.log
 #sleep 10
 
-python3.8 client_jetson_adp.py --ppl 10 --mode bandwidth-aware --config config_jetson2.yaml --log 13_jetson2_wlan_ppl_10_SM_bandwidth_2.log
+python3.8 client_jetson_adp.py --ppl 10 --mode bandwidth-aware --config config_jetson2.yaml --log 13_jetson2_wlan_ppl_10_SM_fixed_end_4_2.log
 sleep 10
 #python3.8 client_jetson_adp.py --ppl 20 --mode bandwidth-aware --config config_jetson2.yaml --log 13_jetson2_wlan_ppl_10_SM_fixed_end_4.log
 #sleep 10
