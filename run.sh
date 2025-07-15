2b5b1611fe0e3a9d43e696665d2af4d863a8ba80
ychen514 <ychen514@uic.edu>
2025-07-14 19:10:22 -0800

    add test

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
   #!/bin/sh
-python3.8 client_jetson_adp.py --ppl 10 --mode default --config config_jetson2.yaml --log test.log
-sleep 10
+#python3.8 client_jetson_adp.py --ppl 10 --mode default --config config_jetson2.yaml --log test.log
+#sleep 10
 
 python3.8 client_jetson_adp.py --ppl 10 --mode bandwidth-aware --config config_jetson2.yaml --log 13_jetson2_wlan_ppl_10_SM_bandwidth_2.log
 sleep 10
