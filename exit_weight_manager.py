be7a39ea866363c83f9ae800a9fe3aa36210cab9
ychen514 <ychen514@uic.edu>
2025-07-17 02:23:09 -0800

    add test

--- exit_weight_manager.py
+++ exit_weight_manager.py
@@ -18,7 +18,7 @@ class ExitWeightManager:
         self.linear_lambda = linear_lambda
         self.mode = mode
 
-    def compute_weight(self,
+    def compute_weight(self, logger,
                         exit_rate,
                         client_compute_time=None,
                         comm_time=None,
@@ -40,11 +40,14 @@ class ExitWeightManager:
                 raise ValueError("Compute time and comm time must be provided in bandwidth-aware mode.")
 
             denom = comm_time + server_compute_time + 1e-6
+            logger.log(f'denom: {denom}')
+            logger.log(f'client_compute_time: {client_compute_time}')
             #comm_factor = client_compute_time / denom
             comm_factor = denom / client_compute_time
+            logger.log(f'comm_factor: {comm_factor}')
+            logger.log(f'exit_rate: {exit_rate}')
             if comm_factor < 8:
                 comm_factor = 1
-
 
             weight = self.lambda_base + (self.lambda_max - self.lambda_base) * exit_rate * comm_factor
             return weight
