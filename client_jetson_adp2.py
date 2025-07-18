a46d559442ace170384f6c424af86dfea928df3a
ychen514 <ychen514@uic.edu>
2025-07-18 13:57:03 -0800

    add test

--- client_jetson_adp2.py
+++ client_jetson_adp2.py
@@ -706,10 +706,9 @@ def task2_computation(models, lm_models, start_idx, end_idx, end_idx_buff, head_
 
 
         # test...
-        end_idx = 1
-        end_idx_buff = 2
+        end_idx = 3
+        end_idx_buff = 4
         # test...
-
         lm_head, _ = get_lm_head_idx(end_idx)
         if not lm_head == head_idx:
             head_idx, lm_models = load_lm_head(args.ckpt_dir_hf_sep, end_idx, device, cache_dir="llm_weights")
@@ -751,8 +750,8 @@ if __name__ == '__main__':
     head_idx = 2
 
     #test...
-    end_idx = 1
-    end_idx_buff = 2
+    end_idx = 3
+    end_idx_buff = 4
     # test...
 
     performance_data_store.statistic_period = 20
