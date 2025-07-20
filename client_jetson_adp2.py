6360b4cb290c163074fb64221aef4705c08c037f
ychen514 <ychen514@uic.edu>
2025-07-19 17:40:45 -0800

    add test

--- client_jetson_adp2.py
+++ client_jetson_adp2.py
@@ -384,7 +384,7 @@ def data_producer(total_batch_num, batch_size, seed, seqlen, bs, tokenizer, mode
 
                     return
 
-            if batch_count == 20:
+            if batch_count == 0:
                 sleep_time_per_layer = 0.3
                 logger.log(f'??????????????????????')
                 logger.log(f'??????????????????????')
@@ -709,10 +709,10 @@ def task2_computation(models, lm_models, start_idx, end_idx, end_idx_buff, head_
 
 
 
-        '''# test...
-        end_idx = 4
-        end_idx_buff = 5
-        # test...'''
+        # test...
+        end_idx = 1
+        end_idx_buff = 2
+        # test...
         lm_head, _ = get_lm_head_idx(end_idx)
         if not lm_head == head_idx:
             head_idx, lm_models = load_lm_head(args.ckpt_dir_hf_sep, end_idx, device, cache_dir="llm_weights")
@@ -753,10 +753,10 @@ if __name__ == '__main__':
     end_idx_buff = args.end_idx_buff
     head_idx = 2
 
-    '''#test...
-    end_idx = 4
-    end_idx_buff = 5
-    # test...'''
+    #test...
+    end_idx = 1
+    end_idx_buff = 2
+    # test...
 
     performance_data_store.statistic_period = 20
     performance_data_store.end_idx = end_idx
