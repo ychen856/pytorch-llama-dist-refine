f0d9454081df67686fb763d17c59ffa1e367a143
ychen514 <ychen514@uic.edu>
2025-07-21 04:21:58 -0800

    add test

--- calculate_edge_opt_adptive.py
+++ calculate_edge_opt_adptive.py
@@ -153,7 +153,7 @@ class PerformanceDataStore:
             target_deque.popleft()  # Remove the oldest if max size reached
         target_deque.append(complete_record)'''
         print('FFFFFFF: ', self.data_storage)
-        if len(self.data_storage[record_type][key]) < self.max_records_per_type + self.statistic_period:
+        if len(self.data_storage) == 0 or len(self.data_storage[record_type][key]) < self.max_records_per_type + self.statistic_period:
             self.data_storage[record_type][key].append(complete_record)
         else:
             self.data_storage[record_type][key].popleft()  # Remove the oldest
