b74c57e72130b9b81e85c33c3610c31ed9bb5cf5
ychen514 <ychen514@uic.edu>
2025-07-29 01:07:21 -0800

    add test

--- llama_edge_server_hf_early_exit_adp.py
+++ llama_edge_server_hf_early_exit_adp.py
@@ -608,7 +608,9 @@ def task2_computation(models, lm_models, start_idx, end_idx, early_idx_buff, end
                 if is_oom:
                     end_idx = min(start_idx + layer_amount, end_idx_temp)
 
-        lm_head, _ = get_lm_head_idx(end_idx)
+        lm_head, _ = utils.get_lm_head_idx(end_idx)
+        logger.log(f'new lm_head: {lm_head}')
+        logger.log(f'old head_idx: {head_idx}')
         if not lm_head == head_idx:
             head_idx, lm_models = load_lm_head(args.ckpt_dir_hf_sep, end_idx, device, cache_dir="llm_weights")
 
