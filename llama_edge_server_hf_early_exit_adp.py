6e5eed24097223d04c52374fa6e65a624857fbc1
ychen514 <ychen514@uic.edu>
2025-07-29 01:27:49 -0800

    add test

--- llama_edge_server_hf_early_exit_adp.py
+++ llama_edge_server_hf_early_exit_adp.py
@@ -468,7 +468,9 @@ def task1_data_sending(args):
         #while outgoing_queue_forward.empty() and incoming_queue.qsize() > 0 and calculate_opt.steady_state:
         #while outgoing_queue.empty() and input_queue.qsize() > 0:
         #while outgoing_queue_forward.qsize() < 3 and incoming_queue.qsize() > 0 and performance_data_store.steady_state:
+        logger.log(f'queue size: {http_receiver.incoming_queue.qsize()}')
         while http_receiver.incoming_queue.qsize() > 0 and performance_data_store.steady_state:
+            logger.log(f'AAAAAAAAAAAAAAAA')
             timeout_count = timeout_count + 1
 
             start_time = time.time()
