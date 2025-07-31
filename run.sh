7a3c12bfb234fcf11d743868e116f07444630f14
ychen514 <ychen514@uic.edu>
2025-07-30 22:40:44 -0800

    add test

--- run.sh
+++ run.sh
@@ -7,8 +7,8 @@
 #python3.8 client_jetson_adp.py --ppl 20 --mode bandwidth-aware --config config_jetson2.yaml --log 13_jetson2_wlan_ppl_10_SM_fixed_end_4.log
 #sleep 10
 #python3.8 client_jetson_adp.py --ppl 30 --mode bandwidth-aware --config config_jetson2.yaml --log 13_jetson2_wlan_ppl_10_SM_fixed_end_4.log
-#sleep 10
-#python3.8 client_jetson_adp2.py --ppl 30 --mode exit-rate --config config_jetson2.yaml --log 25_test_3_1.log
+sleep 10
+python3.8 client_jetson_adp2.py --ppl 30 --mode exit-rate --config config_jetson2.yaml --log 25_test_3_1.log
 
 sleep 10
 python3.8 client_jetson_adp2.py --ppl 30 --mode exit-rate --config config_jetson2.yaml --log 25_test_3_2.log
@@ -16,5 +16,5 @@ python3.8 client_jetson_adp2.py --ppl 30 --mode exit-rate --config config_jetson
 sleep 10
 python3.8 client_jetson_adp2.py --ppl 30 --mode exit-rate --config config_jetson2.yaml --log 25_test_3_3.log
 
-#sleep 10
-#python3.8 client_jetson_adp2.py --ppl 30 --mode exit-rate --config config_jetson2.yaml --log 25_test_3_4.log
+sleep 10
+python3.8 client_jetson_adp2.py --ppl 30 --mode exit-rate --config config_jetson2.yaml --log 25_test_3_4.log
