3b3befd6ffae26cce19533349d3898853f4c7fad
ychen514 <ychen514@uic.edu>
2025-08-02 14:32:48 -0800

    add test

--- run.sh
+++ run.sh
@@ -8,16 +8,16 @@
 #sleep 10
 #python3.8 client_jetson_adp.py --ppl 30 --mode bandwidth-aware --config config_jetson2.yaml --log 13_jetson2_wlan_ppl_10_SM_fixed_end_4.log
 sleep 10
-python3.8 client_jetson_adp2.py --ppl 30 --mode exit-rate --config config_jetson2.yaml --log 25_test_pool_3_1.log
+python3.8 client_jetson_adp2.py --ppl 30 --mode exit-rate --config config_jetson2.yaml --log 25_test_pool_4_1.log
 
 sleep 10
-python3.8 client_jetson_adp2.py --ppl 30 --mode exit-rate --config config_jetson2.yaml --log 25_test_pool_3_2.log
+python3.8 client_jetson_adp2.py --ppl 30 --mode exit-rate --config config_jetson2.yaml --log 25_test_pool_4_2.log
 
 sleep 10
-python3.8 client_jetson_adp2.py --ppl 30 --mode exit-rate --config config_jetson2.yaml --log 25_test_pool_3_3.log
+python3.8 client_jetson_adp2.py --ppl 30 --mode exit-rate --config config_jetson2.yaml --log 25_test_pool_4_3.log
 
 sleep 10
-python3.8 client_jetson_adp2.py --ppl 30 --mode exit-rate --config config_jetson2.yaml --log 25_test_pool_3_4.log
+python3.8 client_jetson_adp2.py --ppl 30 --mode exit-rate --config config_jetson2.yaml --log 25_test_pool_4_4.log
 
 sleep 10
-python3.8 client_jetson_adp2.py --ppl 30 --mode exit-rate --config config_jetson2.yaml --log 25_test_pool_3_5.log
+python3.8 client_jetson_adp2.py --ppl 30 --mode exit-rate --config config_jetson2.yaml --log 25_test_pool_4_5.log
