b640710b4904fd71d9b415da9395e5a360757694
ychen514 <ychen514@uic.edu>
2025-08-06 01:24:28 -0800

    add test

--- http_receiver.py
+++ http_receiver.py
@@ -65,17 +65,17 @@ class S(BaseHTTPRequestHandler):
         start_time = time.time()
         content_length = int(self.headers['Content-Length'])
 
-        '''post_data = self.rfile.read(content_length)
+        post_data = self.rfile.read(content_length)
         decompress_data = lz4.frame.decompress(post_data)
         del post_data
         self._set_headers()
         decrypt_data = pickle.loads(decompress_data)
-        del decompress_data'''
+        del decompress_data
 
-        post_data = self.rfile.read(content_length)
+        '''post_data = self.rfile.read(content_length)
         self._set_headers()
 
-        decrypt_data = pickle.loads(post_data)
+        decrypt_data = pickle.loads(post_data)'''
 
         #if decrypt_data[0] == 'communication':
             #S.sleep_time = decrypt_data[1]
