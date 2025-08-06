514beb8ef9ee5e30c19c1273dcd825dc5f7a94be
ychen514 <ychen514@uic.edu>
2025-08-06 15:09:16 -0800

    add test

--- client_jetson_adp2.py
+++ client_jetson_adp2.py
@@ -24,8 +24,8 @@ from model_hf import LlamaForCausalLM_emb, LlamaForCausalLM_linear, LlamaForCaus
 from data import get_wikitext2_testloader, get_wikitext2_random_test_stream, get_wikitext2_testloader_full
 from timestamp_manager import Timestamp_manager
 from early_exit import early_exit_lm_head, early_exit_regression
-import http_sender
-#import http_sender_2 as http_sender
+#import http_sender
+import http_sender_2 as http_sender
 from logger import Logger
 
 parser = argparse.ArgumentParser(
@@ -531,7 +531,7 @@ def task1_data_sending_direct(args):
 
 
 
-'''def task1_data_sending(args):
+def task1_data_sending_multi(args):
     with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
         futures = []
         while 1 and not stop_event.is_set():
@@ -577,7 +577,7 @@ def task1_data_sending_direct(args):
                 executor.submit(http_sender.send_request, args.gateway_ip, args.gateway_port, data, performance_data_store, timestamp_manager, logger))
 
         # 等所有任務完成
-        concurrent.futures.wait(futures)'''
+        concurrent.futures.wait(futures)
 
 
 '''def task2_computation():
@@ -860,8 +860,9 @@ if __name__ == '__main__':
                                                                                             "distribution": "exponential",
                                                                                             "dist_args": {"scale": 0.8}
                                                                                             })
-    thread1 = threading.Thread(target=task1_data_sending, args=[args])
+    #thread1 = threading.Thread(target=task1_data_sending, args=[args])
     #thread1 = threading.Thread(target=task1_data_sending_direct, args=[args])
+    thread1 = threading.Thread(target=task1_data_sending_multi, args=[args])
     thread2 = threading.Thread(target=task2_computation,
                                args=[models, lm_models, start_idx, performance_data_store.end_idx, performance_data_store.end_idx_buff,
                                      head_idx, max_layers, batch_num, device])
